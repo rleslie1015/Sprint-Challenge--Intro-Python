c4f553d6e924868bea92e19d07c690b0a95aa084
Leslie Rodriguez <rleslie1015@gmail.com>
2020-07-17 20:44:57 -0500

    completes oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -25,31 +25,31 @@ class Vehicle:
 
 class FlightVehicle(Vehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass
 
 class Starship(FlightVehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass
 
 class Airplane(FlightVehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass
 
 class GroundVehicle(Vehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass
 
 class Car(GroundVehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass
 
 
 class Motorcycle(GroundVehicle):
     def __init__(self):
-        # super().__init__(self)
+        super().__init__()
         pass    
